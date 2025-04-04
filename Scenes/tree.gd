2e135f33c5f4cda4a291c7ee72c26b546d8c39cc
Rotaru Floris Adrian <florisadrian18@gmail.com>
2025-04-04 21:29:29 +0300

    artefact use

--- Scenes/tree.gd
+++ Scenes/tree.gd
@@ -1,16 +1,13 @@
 extends CharacterBody2D
 
-var in_range: bool
-
-func _ready() -> void:
-	in_range = false
-
 func _process(delta: float) -> void:
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name.contains("Player"):
+		body.artefact_use();
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name.contains("Player"):
+		body.artefact_dont_use();
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,16 +6,20 @@ var direction := Vector2.ZERO
 
 var artefact: bool
 var can_use_art: bool #when in range of using
+var in_range: bool
+var art_cooldown: bool #false = no cooldown
 
 func _ready() -> void:
 	artefact = true
 	can_use_art = true
+	in_range = false
+	art_cooldown = false
 
 func _physics_process(delta: float) -> void:
 	#Movement
 	direction.x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
 	direction.y = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
-
+	
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
 		velocity = direction * speed
@@ -27,12 +31,19 @@ func _physics_process(delta: float) -> void:
 	global_position = global_position.snapped(Vector2.ONE)
 	
 	#Artefact
+	if in_range and art_cooldown == false:
+		can_use_art = true
+	else:
+		can_use_art = false
+	
 	if artefact == true: #player has artefact in hand
 		if can_use_art == true: #didn't already use or is in range of using
 			print("Press lmb to use")
 			if Input.is_action_just_pressed("lmb"):
 				print("used")
-				can_use_art = false
+				art_cooldown = true
+				$Timer.start(3)
+				print("sdfsdf")
 
 func play_walk_animation(dir: Vector2) -> void:
 	if abs(dir.x) >= abs(dir.y):
@@ -45,3 +56,13 @@ func play_walk_animation(dir: Vector2) -> void:
 			anim_sprite.play("Run_South")
 		else:
 			anim_sprite.play("Run_North")
+
+func artefact_use():
+	in_range = true
+
+func artefact_dont_use():
+	in_range = false
+
+
+func _on_timer_timeout() -> void:
+	art_cooldown = false
