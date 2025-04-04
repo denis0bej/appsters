8561b3b6a124a52668c0c81047f8cd039e843ab7
Rotaru Floris Adrian <florisadrian18@gmail.com>
2025-04-04 23:02:44 +0300

    artefact

--- Scenes/tree.gd
+++ Scenes/tree.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+func _ready() -> void:
+	pass
+
 func _process(delta: float) -> void:
 	pass
 
@@ -7,7 +10,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name.contains("Player"):
 		body.artefact_use();
 
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name.contains("Player"):
 		body.artefact_dont_use();
+
+func _on_tree_bw_anim_animation_looped() -> void:
+	pass
