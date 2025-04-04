4aa189cd66fb05238d54e5848a9a76055a1ceaf9
denis0bej <bejan.topse.denis@gmail.com>
2025-04-04 20:07:11 +0300

    incerc camera

--- Scenes/camera_2d.gd
+++ Scenes/camera_2d.gd
@@ -1,15 +1,29 @@
 extends Camera2D
-var CamLimit1 = Vector2(0,0) #x1 y1 - x2 y2
-var CamLimit2 = Vector2(0,0)
+var hLim1
+var hLim2
+var vertical = false
 @export var target: CharacterBody2D
-func _process(_delta) -> void:
-	if target.position.y >= min(CamLimit1.y,CamLimit2.y) and target.position.y <= max(CamLimit1.y,CamLimit2.y):
-		position = target.position
-func _on_gate_front_body_entered(body: Node2D) -> void:
-	CamLimit1 = $"../CameraAreas/GateFront/pos1".position
-	CamLimit2 = $"../CameraAreas/GateFront/pos2".position
+@export var AreaPosition: Vector2
 
+func _ready() -> void:
+	pass
+func _process(_delta) -> void:
+	if vertical:
+		position.y = clamp(target.position.y,min(hLim1,hLim2),max(hLim1,hLim2))
+	else:
+		position = lerp(position, AreaPosition, 0.1)
+func _on_gate_front_body_entered(body: Node2D) -> void:
+	if body.name.contains("Player"):
+		vertical = false
+		AreaPosition = $"../CameraAreas/GateFront/CollisionShape2D".position
+		print("Entered gate area")
 
 func _on_town_main_road_body_entered(body: Node2D) -> void:
-	CamLimit1 = $"../CameraAreas/TownMainRoad/pos1".position
-	CamLimit2 = $"../CameraAreas/TownMainRoad/pos2".position
+	if body.name.contains("Player"):
+		vertical = true
+		print("Entered town main road")
+		
+		hLim2 = $"../CameraAreas/TownMainRoad/pos1".position.y
+		hLim1 = $"../CameraAreas/TownMainRoad/pos2".position.y
+		print(hLim1)
+		print(hLim2)
