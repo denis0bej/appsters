f89d02661b6b0dc546799f1346d48c002b55b6c4
denis0bej <bejan.topse.denis@gmail.com>
2025-04-06 08:22:09 +0300

    added doggo

--- Scenes/castle_interior.gd
+++ Scenes/castle_interior.gd
@@ -5,9 +5,8 @@ extends Node2D
 @export var knightsAlive = 3
 
 func _physics_process(delta: float) -> void:
-	print(knightsAlive)
 	if knightsAlive == 0:
-		print("DEAD ALL")
+		$AnimationPlayer.play("Victory")
 	if can_move:
 		var direction = (target.global_position - knight.global_position).normalized()
 		knight.global_position += direction * 0.6
--- Scripts/chained_doggy.gd
+++ Scripts/chained_doggy.gd
@@ -1,4 +1,26 @@
 extends CharacterBody2D
+var PlayerInRange = false
+@export var freeable = false
+
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("cry")
+
+func _physics_process(delta: float) -> void:
+	if PlayerInRange and freeable and Input.is_action_just_pressed("rmb"):
+		$AnimatedSprite2D.play("free")
+		await get_tree().create_timer(1).timeout
+		visible=false
+		$"../doggy".visible = true
+		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name.contains("Player"):
+		PlayerInRange = true
+		print("DENIS")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name.contains("Player"):
+		PlayerInRange = false
+		print("Denis left")
